18d0a99f2906b63721fb5287371a4be659080ae7
qucumbah <39967396+qucumbah@users.noreply.github.com>
2022-01-05 11:54:39 +0300

    finish sha-1 implementation

--- lab5/logic/src/sha1.rs
+++ lab5/logic/src/sha1.rs
@@ -1,7 +1,63 @@
 #![allow(dead_code)]
 
 pub fn hash(message: &[u8], hash: &mut [u8; 20]) {
+    let mut h: [u32; 5] = [
+        0x67452301,
+        0xEFCDAB89,
+        0x98BADCFE,
+        0x10325476,
+        0xC3D2E1F0,
+    ];
+
     let padded_message: &[u8] = &pad(message)[..];
+    for chunk_start in (0..padded_message.len()).step_by(64) {
+        let mut w: Vec<u32> = Vec::new();
+        for i in 0..16 {
+            w.push(u32_from_bytes(padded_message, chunk_start + i * 4));
+        }
+
+        for i in 16..80 {
+            w.push(lotr(w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1));
+        }
+
+        let mut a: u32 = h[0];
+        let mut b: u32 = h[1];
+        let mut c: u32 = h[2];
+        let mut d: u32 = h[3];
+        let mut e: u32 = h[4];
+
+        for i in 0..80 {
+            let (f, k): (u32, u32) = if i < 20 {
+                ((b & c) | (!b & d), 0x5A827999)
+            } else if i < 40 {
+                (b ^ c ^ d, 0x6ED9EBA1)
+            } else if i < 60 {
+                ((b & c) | (b & d) | (c & d), 0x8F1BBCDC)
+            } else {
+                (b ^ c ^ d, 0xCA62C1D6)
+            };
+
+            let temp: u32 = lotr(a, 5) + f + e + k + w[i];
+            e = d;
+            d = c;
+            c = lotr(b, 30);
+            b = a;
+            a = temp;
+        }
+
+        h[0] += a;
+        h[1] += b;
+        h[2] += c;
+        h[3] += d;
+        h[4] += e;
+    }
+
+    for i in 0..5 {
+        let h_bytes: Vec<u8> = u32_to_bytes(h[i]);
+        for byte_number in 0..4 {
+            hash[i * 4 + byte_number] = h_bytes[byte_number];
+        }
+    }
 }
 
 fn pad(message: &[u8]) -> Vec<u8> {
@@ -12,11 +68,13 @@ fn pad(message: &[u8]) -> Vec<u8> {
         _ => message.len() / 64 * 64 + 64,
     };
 
+    result.push(0x80);
+
     for _ in result.len()..desired_length - 8 {
         result.push(0);
     }
 
-    result.extend(to_bytes(message.len() as u64));
+    result.extend(u64_to_bytes((message.len() * 8) as u64));
 
     result
 }
@@ -27,11 +85,21 @@ fn array_copy<T: Copy>(src: &[T], src_offset: usize, dst: &mut [T], dst_offset: 
     }
 }
 
-fn to_bytes(src: u64) -> Vec<u8> {
+fn u64_to_bytes(src: u64) -> Vec<u8> {
     let mut result: Vec<u8> = Vec::new();
 
     for i in 0..8 {
         result.push((src >> ((7 - i) * 8)) as u8);
+    }
+
+    result
+}
+
+fn u32_to_bytes(src: u32) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
+
+    for i in 0..4 {
+        result.push((src >> ((3 - i) * 8)) as u8);
     }
 
     result
@@ -78,11 +146,20 @@ mod tests {
     }
 
     #[test]
-    fn to_bytes_test() {
+    fn u64_to_bytes_test() {
         let src = 0x0706050403020100;
-        let dst = super::to_bytes(src);
+        let dst = super::u64_to_bytes(src);
         for i in 0..8 {
             assert_eq!(dst[i], (7 - i) as u8);
+        }
+    }
+
+    #[test]
+    fn u32_to_bytes_test() {
+        let src = 0x03020100;
+        let dst = super::u32_to_bytes(src);
+        for i in 0..4 {
+            assert_eq!(dst[i], (3 - i) as u8);
         }
     }
 
@@ -121,7 +198,7 @@ mod tests {
 
         assert_eq!(padded_message.len(), expected_length);
 
-        let length_bytes = super::to_bytes(message.len() as u64);
+        let length_bytes = super::u64_to_bytes(message.len() as u64);
 
         for i in 0..expected_length - 8 {
             let expected_element = if i < message.len() {
